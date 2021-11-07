21a2ae09d2f8da0e8cba963886035db6ebd3757b
舟匪 <zhoufei@youzhuan.onaliyun.com>
2021-11-07 23:37:30 +0800

    Chore 增加项目环境配置

---
+++ environment.ts
@@ -0,0 +1,7 @@
+export default {
+  development: 'http://127.0.0.1:7788',
+  dev: 'https://dev-mp.zhuawangame.work',
+  test: 'https://test-mp.zhuawangame.work',
+  pre: 'https://pre-mp.zhuawangame.work',
+  prod: 'https://mp.zhuawan.com',
+};
